4e8c71fa774115d5071b4a8803c5a922268e0f6f
aakashranjan811 <86969736+aakashranjan811@users.noreply.github.com>
2021-07-16 20:27:27 +0530

    Update glass_type_app.py

--- glass_type_app.py
+++ glass_type_app.py
@@ -1,9 +1,9 @@
 # Importing the necessary Python modules.
 import numpy as np
 import pandas as pd
-import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
+import streamlit as st
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
@@ -13,13 +13,13 @@ from sklearn.metrics import precision_score, recall_score
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
- 
+
 # Loading the dataset.
 @st.cache()
 def load_data():
     file_path = "glass-types.csv"
     df = pd.read_csv(file_path, header = None)
-    # Dropping the 0th column as it contains only the serial numbers.
+    # Droping the 0th column as it contains only the serial numbers.
     df.drop(columns = 0, inplace = True)
     column_headers = ['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'GlassType']
     columns_dict = {}
@@ -28,6 +28,7 @@ def load_data():
         columns_dict[i] = column_headers[i - 1]
         # Rename the columns.
         df.rename(columns_dict, axis = 1, inplace = True)
+
     return df
 
 glass_df = load_data() 
@@ -40,8 +41,7 @@ y = glass_df['GlassType']
 
 # Spliting the data into training and testing sets.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
-# Create a function that accepts an ML model object say 'model' and the nine features as inputs 
-# and returns the glass type.
+
 @st.cache()
 def prediction(model, ri, na, mg, al, si, k, ca, ba, fe):
     glass_type = model.predict([[ri, na, mg, al, si, k, ca, ba, fe]])
@@ -60,93 +60,136 @@ def prediction(model, ri, na, mg, al, si, k, ca, ba, fe):
         return "tableware".upper()
     else:
         return "headlamps".upper()
+
 # Add title on the main page and in the sidebar.
-st.title("Glass Type prediction Web app")
-st.sidebar.title("Glass Type prediction Web app")
+st.title("Glass Type Predictor")
+st.sidebar.title("Glass Type Features")
+
 # Using if statement, display raw data on the click of the checkbox.
 if st.sidebar.checkbox("Show raw data"):
     st.subheader("Glass Type Data set")
     st.dataframe(glass_df)
-# S1.1: Add a multiselect widget to allow the user to select multiple visualisation.
+
+# Add a multiselect widget to allow the user to select multiple visualisation.
+# Add a subheader in the sidebar with label "Visualisation Selector"
 st.sidebar.subheader("Visualisation Selector")
-plot_list =st.sidebar.multiselect("Select the Charts/Plots:", ('Correlation Heatmap', 'Line Chart', 'Area Chart', 'Count Plot','Pie Chart', 'Box Plot'))
-# S1.2: Display Streamlit native line chart and area chart
-if "Line Chart" in plot_list:
-  st.subheader('Line Chart')
-  st.line_chart(glass_df)
-if "Area Chart" in plot_list:
-  st.subheader('Area Chart')
-  st.area_chart(glass_df)
-# S1.3: Display the plots when the user selects them using multiselect widget.
+
+# Add a multiselect in the sidebar with label 'Select the Charts/Plots:'
+# and with 6 options passed as a tuple ('Correlation Heatmap', 'Line Chart', 'Area Chart', 'Count Plot','Pie Chart', 'Box Plot').
+# Store the current value of this widget in a variable 'plot_list'.
+plot_list = st.sidebar.multiselect("Select the Charts/Plots:", 
+                                    ('Correlation Heatmap', 'Line Chart', 'Area Chart', 
+                                     'Count Plot','Pie Chart', 'Box Plot'))
+
+
+# Display Streamlit native line chart and area chart
+# Display line chart 
+if 'Line Chart' in plot_list:
+    st.subheader("Line Chart")
+    st.line_chart(glass_df)
+
+# Display area chart    
+if 'Area Chart' in plot_list:
+    st.subheader("Area Chart")
+    st.area_chart(glass_df)
+
+# Display the plots when the user selects them using multiselect widget.
+# import 'seaborn' and 'matplotlib.pyplot' module.
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
-if "Correlation Heatmap" in plot_list:
-  st.subheader('Correlation Heatmap')
-  plt.figure(fizsize=(20,5))
-  sns.heatmap(glass_df.corr(),annot=True)
-  st.pyplot()
-if "Count plot" in plot_list:
-  st.subheader('Count plot')
-  plt.figure(fizsize=(20,5))
-  sns.countplot(x=GlassType,data=glass_df)
-  st.pyplot()
-if "Pie Chart" in plot_list:
-  st.subheader('Pie Chart')
-  plt.figure(fizsize=(20,5))
-  pie_data = glass_df['GlassType'].value_counts()
-  plt.pie(pie_data, labels = pie_data.index, autopct = '%1.2f%%', startangle = 30, explode = np.linspace(.06, .12, 6))
-  st.pyplot()
-# S1.4: Display box plot using matplotlib module and 'st.pyplot()'
-if "Box Plot" in plot_list:
-  st.subheader("Box Plot")
-  plt.figure(figsize(20,5))
-  column = st.sidebar.selectbox("Select the column for boxplot",('RI','Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'GlassType'))
-  sns.boxplot(glass_df[column])
-  st.pyplot()
-# S2.1: Add 9 slider widgets for accepting user input for 9 features.
-st.sidebar.subheader('Select Your Values')
-ri = st.sidebar.slider("Input Ri", float(glass_df['RI'].min()),float(glass_df['RI'].max()))
-na = st.sidebar.slider("Input Na", float(glass_df['Na'].min()),float(glass_df['Na'].max()))
-mg = st.sidebar.slider("Input Mg", float(glass_df['Mg'].min()),float(glass_df['Mg'].max()))
-al = st.sidebar.slider("Input Al", float(glass_df['Al'].min()),float(glass_df['Al'].max()))
-sl= st.sidebar.slider("Input Sl", float(glass_df['Sl'].min()),float(glass_df['Sl'].max()))
-k = st.sidebar.slider("Input K", float(glass_df['K'].min()),float(glass_df['K'].max()))
-ca = st.sidebar.slider("Input Ca", float(glass_df['Ca'].min()),float(glass_df['Ca'].max())) 
-ba = st.sidebar.slider("Input Ba", float(glass_df['Ba'].min()),float(glass_df['Ba'].max()))
-fe = st.sidebar.slider("Input Fe", float(glass_df['Fe'].min()),float(glass_df['Fe'].max()))
-# S3.1: Add a subheader and multiselect widget.
-classifier =st.sidebar.selectbox("Choose Classifier", ('Support Vector Machine', 'Random Forest Classifier', 'Logistic Regression'))
-# S4.1: Implement SVM with hyperparameter tuning
+
+# Display correlation heatmap using seaborn module and 'st.pyplot()'
+if 'Correlation Heatmap' in plot_list:
+    st.subheader("Correlation Heatmap")
+    plt.figure(figsize = (9, 5))
+    sns.heatmap(glass_df.corr(), annot = True)
+    st.pyplot()
+
+# Display count plot using seaborn module and 'st.pyplot()' 
+if 'Count Plot' in plot_list:
+    st.subheader("Count plot")
+    sns.countplot(x = 'GlassType', data = glass_df)
+    st.pyplot()
+
+# Display pie chart using matplotlib module and 'st.pyplot()'   
+if 'Pie Chart' in plot_list:
+    st.subheader("Pie Chart")
+    pie_data = glass_df['GlassType'].value_counts()
+    plt.figure(figsize = (5, 5))
+    plt.pie(pie_data, labels = pie_data.index, autopct = '%1.2f%%', 
+            startangle = 30, explode = np.linspace(.06, .16, 6))
+    st.pyplot()
+
+# Display box plot using matplotlib module and 'st.pyplot()'
+if 'Box Plot' in plot_list:
+    st.subheader("Box Plot")
+    column = st.sidebar.selectbox("Select the column for boxplot",
+                                  ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe', 'GlassType'))
+    plt.figure(figsize = (12, 2))
+    sns.boxplot(glass_df[column])
+    st.pyplot()
+
+# Add 9 slider widgets for accepting user input for 9 features.
+st.sidebar.subheader("Select your values:")
+ri = st.sidebar.slider("Input Ri", float(glass_df['RI'].min()), float(glass_df['RI'].max()))
+na = st.sidebar.slider("Input Na", float(glass_df['Na'].min()), float(glass_df['Na'].max()))
+mg = st.sidebar.slider("Input Mg", float(glass_df['Mg'].min()), float(glass_df['Mg'].max()))
+al = st.sidebar.slider("Input Al", float(glass_df['Al'].min()), float(glass_df['Al'].max()))
+si = st.sidebar.slider("Input Si", float(glass_df['Si'].min()), float(glass_df['Si'].max()))
+k = st.sidebar.slider("Input K", float(glass_df['K'].min()), float(glass_df['K'].max()))
+ca = st.sidebar.slider("Input Ca", float(glass_df['Ca'].min()), float(glass_df['Ca'].max()))
+ba = st.sidebar.slider("Input Ba", float(glass_df['Ba'].min()), float(glass_df['Ba'].max()))
+fe = st.sidebar.slider("Input Fe", float(glass_df['Fe'].min()), float(glass_df['Fe'].max()))
+
+# Add a subheader and multiselect widget.
+# Add a subheader in the sidebar with label "Choose Classifier"
+st.sidebar.subheader("Choose Classifier")
+
+# Add a selectbox in the sidebar with label 'Classifier'.
+# and with 2 options passed as a tuple ('Support Vector Machine', 'Random Forest Classifier').
+# Store the current value of this slider in a variable 'classifier'.
+classifier = st.sidebar.selectbox("Classifier", 
+                                 ('Support Vector Machine', 'Random Forest Classifier', 'Logistic Regression'))
+
+# Implement SVM with hyperparameter tuning
+# Import 'plot_confusion_matrix' module.
 from sklearn.metrics import plot_confusion_matrix
+
+
+# if classifier == 'Support Vector Machine', ask user to input the values of 'C','kernel' and 'gamma'.
 if classifier == 'Support Vector Machine':
-  st.sidebar.subheader('Model Hyperparameters')
-  c_value = st.sidebar.number_input("C (Error Rate)",1,100,step =1)
-  kernal_input =st.sidebar.radio("Kernal",("linear",'rbf','ploy'))
-  gammma_input =st.sidebar.number_input('Gamma',1,100,step =1)
-  if st.sidebar.button('classify'):
-    st.subheader('Support Vector Machine')
-    svc_model=SVC(C = c_value, kernel = kernel_input, gamma = gamma_input)
-    svc_model.fit(X_train,y_train)
-    y_pred = svc_model.predict(X_test)
-    accuracy = svc_model.score(X_test, y_test)
-    glass_type = prediction(svc_model, ri, na, mg, al, si, k, ca, ba, fe)
-    st.write("The Type of glass predicted is:", glass_type)
-    st.write("Accuracy", accuracy.round(2))
-    plot_confusion_matrix(svc_model, X_test, y_test)
-    st.pyplot()
-# S5.1: ImplementRandom Forest Classifier with hyperparameter tuning.
-from sklearn.metrics import plot_confusion_matrix
-if classifier == 'RandomForestClassifier':
-  st.sidebar.subheader('Model Hyperparameters')
-  n_estimators_input = st.sidebar.number_input("Number of trees in the forest", 100, 5000, step = 10)
-  max_depth_input = st.sidebar.number_input("Maximum depth of the tree", 1,00, step = 1)
-  if st.sidebar.button('classify'):
-    st.subheader('RandomForestClassifier')
-    rf=RandomForestClassifier(n_estimater = n_estimators_input,max_depth = max_depth_input,n_jobs = -1)
-    rf.fit(X_train,y_train)
-    y_pred = rf.predict(X_test)
-    accuracy = rf.score(X_test, y_test)
-    glass_type = prediction(rf, ri, na, mg, al, si, k, ca, ba, fe)
-    st.write("The Type of glass predicted is:", glass_type)
-    st.write("Accuracy", accuracy.round(2))
-    plot_confusion_matrix(rf, X_test, y_test)
-    st.pyplot()
+    st.sidebar.subheader("Model Hyperparameters")
+    c_value = st. sidebar.number_input("C (Error Rate)", 1, 100, step = 1)
+    kernel_input = st.sidebar.radio("Kernel", ("linear", "rbf", "poly"))
+    gamma_input = st. sidebar.number_input("Gamma", 1, 100, step = 1)
+
+    if st.sidebar.button('Classify'):
+        st.subheader("Support Vector Machine")
+        svc_model=SVC(C = c_value, kernel = kernel_input, gamma = gamma_input)
+        svc_model.fit(X_train,y_train)
+        y_pred = svc_model.predict(X_test)
+        accuracy = svc_model.score(X_test, y_test)
+        glass_type = prediction(svc_model, ri, na, mg, al, si, k, ca, ba, fe)
+        st.write("The Type of glass predicted is:", glass_type)
+        st.write("Accuracy", accuracy.round(2))
+        plot_confusion_matrix(svc_model, X_test, y_test)
+        st.pyplot()
+
+# Random Forest Classifier
+if classifier =='Random Forest Classifier':
+    st.sidebar.subheader("Model Hyperparameters")
+    n_estimators_input = st.sidebar.number_input("Number of trees in the forest", 100, 5000, step = 10)
+    max_depth_input = st.sidebar.number_input("Maximum depth of the tree", 1, 100, step = 1)
+        
+    if st.sidebar.button('Classify'):
+        st.subheader("Random Forest Classifier")
+        rf_clf= RandomForestClassifier(n_estimators = n_estimators_input, max_depth = max_depth_input, n_jobs = -1)
+        rf_clf.fit(X_train,y_train)
+        accuracy = rf_clf.score(X_test, y_test)
+        glass_type = prediction(rf_clf, ri, na, mg, al, si, k, ca, ba, fe)
+        st.write("The Type of glass predicted is:", glass_type)
+        st.write("Accuracy", accuracy.round(2))
+        plot_confusion_matrix(rf_clf, X_test, y_test)
+        st.pyplot()
